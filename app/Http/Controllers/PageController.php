19918036845157a69309c865bdced5ea81df1dd0
Volkan <volkanhmo@gmail.com>
2017-05-07 16:38:27 +0200

    Initial commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Volkan
+ * Date: 07.05.2017
+ * Time: 13:31
+ */
+
+namespace App\Http\Controllers;
+
+
+class PageController
+{
+
+}
---
+++ resources/views/kontakt.blade.php
@@ -0,0 +1,9 @@
+XHTML
+
+@extends('app')
+
+@section('content')
+    <div class="container">
+        <h1>Impressum</h1>
+    </div>
+@endsection
